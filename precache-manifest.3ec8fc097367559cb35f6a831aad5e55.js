e9fc10768d6aff66d7d2e5ca7615ff7979457b9e
sandrosc <sandro@012345.ch>
2020-04-04 14:17:24 +0000

    sandrosc published a site update

---
+++ precache-manifest.3ec8fc097367559cb35f6a831aad5e55.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0fc976cb61de77e40b5be24cb62b35c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0cb66fb6b000faeb0746",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "616cd23d453136eca244",
+    "url": "/static/js/2.3b9726bd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3b9726bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0cb66fb6b000faeb0746",
+    "url": "/static/js/main.a7b7836c.chunk.js"
+  },
+  {
+    "revision": "1f8eacf4b457cf2bbb8e",
+    "url": "/static/js/runtime-main.7522e07d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
